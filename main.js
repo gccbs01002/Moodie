d7ecb8c2c88f361984e5e4dc7f4292045af91f68
star <gccbs01002@gmail.com>
2025-09-02 04:02:31 +0800

    first commit

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-import "./assets/scss/all.scss";
+import "/assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 console.log("Hello world");
 
-const BASE = import.meta.env.BASE_URL; // dev: '/', prod: '/Moodie/'
-link.href = BASE + "pages/recommend/recom-excit.html";
+const page = document.body.dataset.page;
 
-export default defineConfig({
-  base: "/Moodie/",
-  server: {
-    open: "/Moodie/pages/recommend/recom-excit.html", // 換成你要的頁
-  },
-  // build.rollupOptions.input 只影響 build，不影響 dev
-});
+async function loadPageScripts() {
+  if (page === 'recom-excit') {
+    await import(new URL('./js/excite-gallery.js', import.meta.url));
+    await import(new URL('./js/slide-overlay.js', import.meta.url));
+  }
+}
+loadPageScripts();
